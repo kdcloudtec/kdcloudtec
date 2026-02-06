bce2bd27fac1bd8b26aba366f8855ef5bc5808e4
kdcloudtec <kdcloudtec@gmail.com>
2026-02-06 12:53:27 +0530

    network

---
+++ backend.tf
@@ -0,0 +1,10 @@
+terraform {
+   backend "s3" {
+     bucket         = "deva-tfstate"   # Make sure this bucket already exists
+     key            = "network/state.tfstate"
+     region         = "us-east-1"
+     encrypt        = true
+     use_lockfile   = true  # replaces dynamodb_table (deprecated)
+   }
+ }
+
