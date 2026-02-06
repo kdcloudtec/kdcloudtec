183b8392ce710c33a167e2f7f0e115e36df12354
kdcloudtec <kdcloudtec@gmail.com>
2026-02-06 14:06:35 +0530

    nework

--- network.tf
+++ network.tf
@@ -4,7 +4,7 @@ resource "aws_vpc" "myvpc" {
    enable_dns_hostnames = true
    tags = {
      terraform = "true"
-     Name = "vpc01"
+     Name = "vpc-01"
    }
  }
 
@@ -15,7 +15,7 @@ resource "aws_subnet" "public" {
   availability_zone       = "us-east-1a"
 
   tags = {
-    Name = "public-subnet"
+    Name = "public-subnet-01"
   }
 }
 
@@ -26,7 +26,7 @@ resource "aws_subnet" "private" {
   availability_zone       = "us-east-1b"
 
   tags = {
-    Name = "private-subnet"
+    Name = "private-subnet-01"
   }
 }
 
